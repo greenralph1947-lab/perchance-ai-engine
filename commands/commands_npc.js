ebfc2076c1411e1bf869ed8e015a7be6a5e83a67
greenralph1947-lab <greenralph1947@gmail.com>
2026-01-21 12:11:39 -0800

    Update commands_npc.js

--- commands/commands_npc.js
+++ commands/commands_npc.js
@@ -1,1 +1,26 @@
+export function register(aiRPG){
+  return function(msgContent){
+    if(msgContent.startsWith("/npc-add ")){
+      const args = msgContent.replace("/npc-add ","").split(" ");
+      const name = args[0];
+      const genSkills = args[1].toLowerCase() === "yes";
+      const prompt = args.slice(2).join(" ");
+      aiRPG.npcEngine.add(name, genSkills, prompt);
+      return true;
+    }
+
+    if(msgContent.startsWith("/npc-list")){
+      aiRPG.npcEngine.list();
+      return true;
+    }
+
+    if(msgContent.startsWith("/npc-edit ")){
+      const [id, newName] = msgContent.replace("/npc-edit ","").split(" ");
+      aiRPG.npcEngine.editName(id, newName);
+      return true;
+    }
+
+    return false;
+  }
+}
 
